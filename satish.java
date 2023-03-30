277990d78d782bfd11177a9fdf699ed818511a76
Satish Zore <satishzore056@gmail.com>
2023-03-30 12:14:53 +0530

    first commit

--- satish.java
+++ satish.java
@@ -2,5 +2,7 @@ public class satish
 {
   public static void main(String[] args) {
      System.out.println("Hello");
+     System.out.println("This is satish");
+     System.out.println("Welcome");
  }
 }
